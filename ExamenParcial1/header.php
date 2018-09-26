980678eceee5374560805d3bb84f5c4e35851541
Luis Enrique Gómez Córdova <39067933+1330494@users.noreply.github.com>
2018-09-26 15:49:32 -0500

    Add files via upload

---
+++ ExamenParcial1/header.php
@@ -0,0 +1,10 @@
+<div style="width: 100%; height: auto; background-color: #DEF;">
+	<nav style="align-content: center;">
+		<ul style="align-items: flex-end;">
+			<li><a href="index.php">Inicio</a></li>
+			<li><a href="nuevo-alumno.php">+ Alumno</a></li>
+			<li><a href="nuevo-tutor.php">+ Tutor</a></li>
+			<li><a href="nueva-carrera.php">+ Carrera</a></li>
+		</ul>
+	</nav>
+</div>
